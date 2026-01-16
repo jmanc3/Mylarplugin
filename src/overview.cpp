6e86348c37ebcda4e78de9a45503176cda3b7c38
jmanc3 <jman12316@live.com>
2026-01-15 21:41:17 -0600

    Reduce first frame flicker

--- src/overview.cpp
+++ src/overview.cpp
@@ -4,7 +4,7 @@
 #include "hypriso.h"
 #include "layout_thumbnails.h"
 #include <climits>
-#include <cmath>
+#include <math.h>
 
 struct OverviewData : UserData {
     std::vector<int> order;
@@ -16,11 +16,19 @@ static float overview_anim_time = 300.0f;
 // {"anchors":[{"x":0,"y":1},{"x":0.4,"y":0.4},{"x":1,"y":0}],"controls":[{"x":0.25099658672626207,"y":0.7409722222222223},{"x":0.6439499918619792,"y":0.007916683620876747}]}
 static std::vector<float> slidetopos2 = { 0, 0.017000000000000015, 0.03500000000000003, 0.05400000000000005, 0.07199999999999995, 0.09199999999999997, 0.11099999999999999, 0.132, 0.15200000000000002, 0.17400000000000004, 0.19599999999999995, 0.21899999999999997, 0.242, 0.266, 0.29100000000000004, 0.31699999999999995, 0.344, 0.372, 0.4, 0.43000000000000005, 0.46099999999999997, 0.494, 0.527, 0.563, 0.6, 0.626, 0.651, 0.675, 0.6970000000000001, 0.719, 0.739, 0.758, 0.777, 0.794, 0.8109999999999999, 0.8260000000000001, 0.841, 0.855, 0.868, 0.881, 0.892, 0.903, 0.914, 0.923, 0.9319999999999999, 0.9410000000000001, 0.948, 0.955, 0.962, 0.968, 0.973, 0.978, 0.983, 0.986, 0.99, 0.993, 0.995, 0.997, 0.998, 0.999, 1 };
 
-void screenshot_loop() {
+void screenshot_loop(std::vector<int> order) {
     running = true;
     later(1000.0f / 24.0f, [](Timer *t) {
         t->keep_running = running;
         hypriso->screenshot_all();
+    });
+    later(1000.0f / 60.0f, [order](Timer *t) {
+        t->keep_running = running;
+        for (int i = 0; i < 3; i++) {
+            if (i < order.size()) {
+                hypriso->screenshot(order[i]);
+            }
+        }
     });
 }
 
@@ -81,7 +89,8 @@ void paint_over_wallpaper(Container *actual_root, Container *c, int monitor, lon
     border(b, {1, 1, 1, .1}, 1, 0, 14 * s); 
 }
 
-void overview::open(int monitor) {
+
+void actual_open(int monitor) {
     hypriso->whitelist_on = true;
     
     auto over = actual_root->child(::absolute, FILL_SPACE, FILL_SPACE);
@@ -99,13 +108,7 @@ void overview::open(int monitor) {
     over->when_mouse_down = nullptr;
     over->when_clicked = nullptr;
     over->when_mouse_up = nullptr;
-    later_immediate([monitor](Timer *) { 
-        screenshotting_wallpaper = true;
-        hypriso->screenshot_wallpaper(monitor);
-        screenshotting_wallpaper = false;
-        hypriso->screenshot_all(); 
-    });
-    screenshot_loop();
+    screenshot_loop(overview_data->order);
     auto creation_time = get_current_time_in_ms();
     
     over->when_paint = [monitor, creation_time](Container *actual_root, Container *c) {
@@ -199,6 +202,17 @@ void overview::open(int monitor) {
     };
     hypriso->damage_entire(monitor);
     hypriso->all_lose_focus();
+}
+
+void overview::open(int monitor) {
+    later_immediate([monitor](Timer *) {
+        screenshotting_wallpaper = true;
+        hypriso->screenshot_wallpaper(monitor);
+        screenshotting_wallpaper = false;
+        hypriso->screenshot_all(); 
+
+        actual_open(monitor);
+    });
 }
 
 void overview::close() {
