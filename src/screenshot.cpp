3abb99f5e2d0955f1770b4b6daa5e595a38d4bc3
jmanc3 <jman12316@live.com>
2026-03-18 16:36:51 -0500

    Screenshot buttons

--- src/screenshot.cpp
+++ src/screenshot.cpp
@@ -3,44 +3,109 @@
 #include "heart.h"
 #include "overview.h"
 
+void label(Container *parent, std::string icon, std::string text) {
+    static float label_h = 20;
+    static float pad = 10;
+    
+    auto full = parent->child(0, 0);
+    full->parent_bounds_limit_input_bounds = false;
+    full->pre_layout = [icon, text](Container *actual_root, Container *c, const Bounds &b) {
+        auto root = get_rendering_root();
+        if (!root) return;
+        auto [rid, s, stage, active_id] = roots_info(actual_root, root);
+        if (rid != hypriso->monitor_from_cursor())
+            return;
+        auto info = gen_text_texture("Segoe Fluent Icons", icon, label_h * s, {1, 1, 1, 1});
+        free_text_texture(info.id); 
+        auto info2 = gen_text_texture(mylar_font, text, label_h * s, {1, 1, 1, 1});
+        free_text_texture(info2.id); 
+        c->wanted_bounds.w = info.w + info2.w + pad * s;
+        c->wanted_bounds.h = std::max(info.h, info2.h) + pad * s;
+    };
+    full->when_paint = [icon, text](Container *actual_root, Container *c) {
+        auto root = get_rendering_root();
+        if (!root) return;
+        auto [rid, s, stage, active_id] = roots_info(actual_root, root);
+        renderfix
+
+        if (c->state.mouse_pressing) {
+            rect(c->real_bounds, {.4, .4, .4, .7}, 0, 0, 2.0, true);
+        } else if (c->state.mouse_hovering) {
+            rect(c->real_bounds, {.3, .3, .3, .7}, 0, 0, 2.0, true);
+        } else {
+            rect(c->real_bounds, {.2, .2, .2, .7}, 0, 0, 2.0, true);
+        }
+        float xoff = pad * s * 2;
+        {
+            auto info = gen_text_texture("Segoe Fluent Icons", icon, label_h * s, {1, 1, 1, 1});
+            draw_texture(info, {c->real_bounds.x + pad * s + xoff, 
+                               c->real_bounds.y + c->real_bounds.h * .5 - info.h * .5, 
+                               (double) info.w, (double) info.h});
+            xoff += info.w;
+            free_text_texture(info.id); 
+        }
+        xoff += 8 * s;
+        {
+            auto info = gen_text_texture(mylar_font, text, label_h * s, {1, 1, 1, 1});
+            draw_texture(info, {c->real_bounds.x + pad * s + xoff, 
+                               c->real_bounds.y + c->real_bounds.h * .5 - info.h * .5, 
+                               (double) info.w, (double) info.h});
+            free_text_texture(info.id); 
+        }
+    };
+}
+
 void actual_open_screenshot_tool() {
     auto tool = actual_root->child(::absolute, FILL_SPACE, FILL_SPACE);
     tool->custom_type = (int) TYPE::SCREENSHOT_TOOL;
     tool->automatically_paint_children = false;
     consume_everything(tool);
 
-    auto type = tool->child(FILL_SPACE, FILL_SPACE);
+    auto type = tool->child(::hbox, FILL_SPACE, FILL_SPACE);
     type->when_paint = [](Container *actual_root, Container *c) {
         auto root = get_rendering_root();
         if (!root) return;
         auto [rid, s, stage, active_id] = roots_info(actual_root, root);
         renderfix
-        
-        if (c->state.mouse_pressing) {
-            rect(c->real_bounds, {0, 0, 1, 1});
-        } if (c->state.mouse_hovering) {
-            rect(c->real_bounds, {0, 1, 0, 1});
-        } else {
-            rect(c->real_bounds, {1, 0, 0, 1});
-        }
+        rect(c->real_bounds, {1, 0, 0, 1});
     };
-    
+    type->spacing = 20;
+
+    label(type, "\uEC4E", "Full screen");
+    label(type, "\uF407", "Rectangle");
+    label(type, "\uECE9", "Window");
+    label(type, "\uF408", "Freeform");
+
     tool->pre_layout = [](Container *actual_root, Container *c, const Bounds &b) {
         auto root = get_rendering_root();
         if (!root) return;
         auto [rid, s, stage, active_id] = roots_info(actual_root, root);
+        if (rid != hypriso->monitor_from_cursor())
+            return;
 
         c->real_bounds = bounds_monitor(rid);
+        c->wanted_bounds = c->real_bounds;
 
         auto type = c->children[0];
-        type->real_bounds = c->real_bounds;
-        type->real_bounds.w = 200;
-        type->real_bounds.h = 50;
-        type->real_bounds.x += c->real_bounds.w * .5 - type->real_bounds.w * .5;
-        type->real_bounds.y += c->real_bounds.h * .9 - type->real_bounds.h * .5;
-        type->wanted_bounds = type->real_bounds;
-        type->wanted_bounds.x = 0;
-        type->wanted_bounds.y = 0;
+        auto fixed_w = (type->wanted_pad.x + type->wanted_pad.w + (type->spacing * type->children.size() - 1));
+        auto fixed_h = type->wanted_pad.y + type->wanted_pad.h;
+        float calc_w = 0;
+        float calc_h = 0;
+        for (auto ch : type->children) {
+            if (ch->pre_layout) {
+                ch->pre_layout(actual_root, ch, b);
+                calc_w += ch->wanted_bounds.w;
+                if (calc_h > ch->wanted_bounds.h)
+                    calc_h = ch->wanted_bounds.h;
+            }
+        }
+        auto final_w = fixed_w + calc_w;
+        auto final_h = fixed_h + calc_h;
+        type->wanted_bounds = Bounds(
+            c->real_bounds.x + c->real_bounds.w * .5 - final_w * .5, 
+            c->real_bounds.y + c->real_bounds.h * .86 - final_h * .5, 
+            final_w, final_h);
+        type->real_bounds = type->wanted_bounds;
         layout(actual_root, type, type->real_bounds);
     };
     tool->when_paint = [](Container *actual_root, Container *c) {
